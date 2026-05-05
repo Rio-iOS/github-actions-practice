d50a0594300618027268c032b44f6d24acd33832
Rio-iOS <rio.2014.swift.ios@gmail.com>
2026-05-05 17:44:48 +0900

    add: Package.swiftにswift-algorithmsを追加

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "github-actions-practice",
+    dependencies: [
+        .package(url:"https://github.com/apple/swift-algorithms", exact: "1.2.1")
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
