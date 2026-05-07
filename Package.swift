5460c963ce074fd6781aab93341d58db3cea055a
Rio-iOS <rio.2014.swift.ios@gmail.com>
2026-05-07 14:17:56 +0900

    update: package.swift

--- Package.swift
+++ Package.swift
@@ -6,13 +6,18 @@ import PackageDescription
 let package = Package(
     name: "github-actions-practice",
     dependencies: [
-        .package(url:"https://github.com/apple/swift-algorithms", exact: "1.2.1")
+        .package(url:"https://github.com/apple/swift-algorithms", from: "1.2.0"),
+        .package(url:"https://github.com/apple/swift-collections", from: "1.1.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .executableTarget(
-            name: "github-actions-practice"
+            name: "github-actions-practice",
+            dependencies: [
+                .product(name: "Algorithms", package: "swift-algorithms"),
+                .product(name: "Collections", package: "swift-collections")
+            ]
         ),
         .testTarget(
             name: "github-actions-practiceTests",
